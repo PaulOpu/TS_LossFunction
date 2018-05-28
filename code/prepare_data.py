a7949d6a674b28be3fb955f764a34308ab025076
Opuchlich, Paul <paul.opuchlich@sap.com>
2018-05-28 15:25:33 +0200

    Delete Twins (sentences that are equal in normal and simple)

--- code/prepare_data.py
+++ code/prepare_data.py
@@ -1,7 +1,10 @@
-def get_aligned_sentences(simple_path,normal_path):
+def get_aligned_sentences(simple_path,normal_path,twins=True):
     sdata = read_file(simple_path)
     ndata = read_file(normal_path)
-    return zip(*[sdata,ndata])
+    aligned_data = zip(*[sdata,ndata])
+    if twins:
+        aligned_data = del_twins(aligned_data)
+    return aligned_data
 
 
 def read_file(path):
@@ -10,3 +13,7 @@ def read_file(path):
         data = data.split("\n")
         data = [line.split("\t") for line in data]
         return data
+
+
+def del_twins(aligned_data):
+    return [ss for ss,ns in aligned_data if ss[-1] != ns[-1]]
