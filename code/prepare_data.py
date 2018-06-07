789a569d2d083319f2005d30f75f8f19fd3beadf
Opuchlich, Paul <paul.opuchlich@sap.com>
2018-06-07 12:15:57 +0200

    Create POS Tags and Tokenizing for British Council Articles

--- code/prepare_data.py
+++ code/prepare_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 
 def get_aligned_sentences(simple_path,normal_path,twins=True):
     sdata = read_file(simple_path)
@@ -25,3 +26,13 @@ def create_df(aligned_data):
     data = [(st,sn,ss,nt,nn,ns) for (st,sn,ss),(nt,nn,ns) in aligned_data]
     df = pd.DataFrame(data=list(data),columns=["simple_topic","simple_numb","simple_sentence","normal_topic","normal_numb","normal_sentence"])
     return df
+
+def load_df(path,list_cols):
+    #list_cols: which columns have lists inside and have to be reparced
+    df = pd.read_csv(path,sep="|")
+    df[list_cols] = df[list_cols].applymap(
+    lambda x: ast.literal_eval(x))
+    return df
+
+def save_df(path,df):
+    df.to_csv(path,sep="|",index=False)
